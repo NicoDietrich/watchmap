27fd0bc4b97b24061a52f69e84b3aed05462741f
Nicolas Dietrich <ndietric@mathematik.uni-kl.de>
2021-05-16 18:00:41 +0200

    rewrote some code, different approach w.r.t. extracting the data

--- plot.py
+++ plot.py
@@ -1,243 +1,211 @@
-#!/usr/bin/python3
-
 import argparse
 import subprocess
 import os
+import numpy as np
+from geopy.distance import distance
 
 from xml.dom import minidom
+import xml.etree.ElementTree as ET
+
 import matplotlib
 import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+
 import folium
 from folium.features import DivIcon
 from datetime import datetime, timedelta
 
-# snippets of the gpx parsing code came from gpxplotter (https://github.com/andersle/gpxplotter)
-def _get_gpx_text(track, tagname, type="str"):
-    """Grab text from a given track."""
-    tag_txt = []
-    tag = track.getElementsByTagName(tagname)
-    for i in tag:
-        for child in i.childNodes:
-            if child.nodeType == child.TEXT_NODE:
-                if type == "float":
-                    tag_txt.append(
-                        float(child.data)
-                    )
-                else:
-                    tag_txt.append(
-                        child.data
-                    )
-    return tag_txt
 
-def _get_gpx_attribute(track, attribute):
-    tag_txt = []
-    tag = track.getElementsByTagName('trkpt')
-    for t in tag:
-        tag_txt.append(
-            float(t.getAttribute(attribute))
-        )
-    return tag_txt
-
-def _get_gpx_hr(track):
-    tag_txt = []
-    tag = track.getElementsByTagName('gpxtpx:hr')
-    for t in tag:
-        for child in t.childNodes:
-            ext = child.getElementsByTagName('gpxtpx:hr')
-            if ext == child.TEXT_NODE:
-                tag_txt.append(float(child.data))
+class TrackData:
+    def __init__(self):
+        self.segments = []
 
 
-def read_gpx_file(gpxfile):
-    gpx = minidom.parse(gpxfile)
-    tracks = gpx.getElementsByTagName('trk')
-    for track in tracks:
-        track.getElementsByTagName('trkseg')
-        track_data = {
-            'speed': _get_gpx_text(track, 'speed', 'float'),
-            'elevation': _get_gpx_text(track, 'ele'),
-            'time': _get_gpx_text(track, 'time'),
-            'lat': _get_gpx_attribute(track, 'lat'),
-            'lon': _get_gpx_attribute(track, 'lon'),
-        }
-        yield track_data
-
-def read_hr_bodge(hrfile):
-    gpx = minidom.parse(hrfile)
-    tracks = gpx.getElementsByTagName('trk')
-    for track in tracks:
-        track.getElementsByTagName('trkseg')
-        track_data = {
-            'hr': _get_gpx_text(track, 'gpxtpx:hr', 'float'),
-            'lat': _get_gpx_attribute(track, 'lat'),
-            'lon': _get_gpx_attribute(track, 'lon'),
-        }
-
-    # returns only the last track
-    return track_data
-
-def speed_conversion(raw):
-    return raw * 1.60934 * 2.0 # convert mph to kph, plus some scaling fudge factor
-
-def plot_osm_map(track, output='speed-map.html', hr=None):
-    for i in range(len(track['speed'])):
-        track['speed'][i] = speed_conversion(track['speed'][i])
-    speeds = track['speed']
-    minima = min(speeds)
-    maxima = max(speeds)
-
-    norm = matplotlib.colors.Normalize(vmin=minima, vmax=maxima, clip=True)
-    mapper = cm.ScalarMappable(norm=norm, cmap=cm.plasma)
-    m = folium.Map(location=[track['lat'][0], track['lon'][0]], zoom_start=15)
-    for index in range(len(track['lat'])):
-        if track['speed'][index] == 0:
-            track['speed'][index] = 0.01
-
-        if hr:
-            try:
-                tooltip="{:0.1f}kph".format(track['speed'][index]) + ' ' + str(hr['hr'][index]) +'bpm'
-            except:
-                tooltip="{:0.1f}kph".format(track['speed'][index])
-        else:
-            tooltip=str(track['speed'][index])
-        folium.CircleMarker(
-            location=(track['lat'][index], track['lon'][index]),
-            radius=track['speed'][index]**2 / 8,
-            tooltip=tooltip,
-            fill_color=matplotlib.colors.to_hex(mapper.to_rgba(track['speed'][index])),
-            fill=True,
-            fill_opacity=0.2,
-            weight=0,
-        ).add_to(m)
-
-    m.save(output)
+class SegmentData:
+    def __init__(self):
+        self.datapoints = []
 
 
-def plot_osm_hr_map(track, hr_file, output='hr-map.html', age=45, resting_rate=50, hr_plot_interval=30):
-    # speeds will have already been adjusted since we side-effect the global record
-#    for i in range(len(track['speed'])):
-#        track['speed'][i] = speed_conversion(track['speed'][i])
+def _get_waypoint_data(waypoint):
+    lap_string = waypoint.getElementsByTagName('name')[0].firstChild.data  # only one result
+    lapnumber = lap_string.replace('LAP', '')
+    lat = waypoint.getAttribute('lat')
+    lon = waypoint.getAttribute('lon')
+    data = {'LAP': lapnumber,
+            'lat': lat,
+            'lon': lon}
+    return data
 
-    maxrate = 220-age
-    reserve = maxrate-resting_rate
-    rate_table = {
-       'resting  ' : [(0.0*reserve + resting_rate, 0.5*reserve + resting_rate), 0],
-       'easy     ' : [(0.5*reserve + resting_rate, 0.6*reserve + resting_rate), 0],
-       'fatburn  ' : [(0.6*reserve + resting_rate, 0.70*reserve + resting_rate), 0],
-       'cardio   ' : [(0.70*reserve + resting_rate, 0.80*reserve + resting_rate), 0],
-       'sprint   ' : [(0.80*reserve + resting_rate, 0.90*reserve + resting_rate), 0],
-       'anaerobic' : [(0.9*reserve + resting_rate, 1.0*reserve + resting_rate), 0],
+
+def get_waypoints_from_gpx(gpxfile):
+    waypointdata = []
+    for waypoint in minidom.parse(gpxfile).getElementsByTagName("wpt"):
+        waypointdata.append(_get_waypoint_data(waypoint))
+    return waypointdata
+
+
+def _merge(file1, file2, outfile):
+    "merges two gpx files, generated by different babel flags, into one"
+    gpx1 = minidom.parse(file1)
+    gpx2 = minidom.parse(file2)
+    root = ET.Element("gpx", {'creator': "me", 'prog_used': "gpsbabel"})
+    for waypoint in gpx1.getElementsByTagName('wpt'):
+        _ = ET.SubElement(root, 'wpt', _get_waypoint_data(waypoint))
+
+    outstr = ET.tostring(root)
+    prettyxml = minidom.parseString(outstr)
+    with open(outfile, "w") as f:
+        f.write(prettyxml.toprettyxml(indent='\t', newl='\n'))
+
+
+def convert_fit_to_gpx(fitfile):
+    speedfile = '/tmp/speed.gpx'
+    hrfile = '/tmp/hr.gpx'
+    subprocess.call(["gpsbabel", "-t", "-i", "garmin_fit", "-x", "track,speed", "-f", fitfile,
+                     "-o", "gpx", "-F", speedfile])
+    subprocess.call(["gpsbabel", "-t", "-i", "garmin_fit", "-f", fitfile,
+                     "-o", "gpx,garminextensions", "-F", hrfile])
+    _merge(speedfile, hrfile, fitfile.replace('.fit', '.gpx'))
+
+
+def process_gpx_file(filename):
+    gpx = minidom.parse(filename)
+    tracks = []
+    for track in gpx.getElementsByTagName('trk'):
+        tdata = TrackData()
+        for tracksegment in track.getElementsByTagName('trkseg'):
+            seg = SegmentData()
+            for trackpoint in tracksegment.getElementsByTagName('trkpt'):
+                seg.datapoints.append(get_data_from_point(trackpoint))
+            tdata.segments.append(seg)
+        tracks.append(tdata)
+    return tracks
+
+
+def get_data_from_point(trackpoint):
+    lat = trackpoint.getAttribute('lat')
+    lon = trackpoint.getAttribute('lon')
+    ele = trackpoint.getElementsByTagName('ele')[0].firstChild.data
+    time = trackpoint.getElementsByTagName('time')[0].firstChild.data
+    speed = trackpoint.getElementsByTagName('speed')[0].firstChild.data
+    atemp = trackpoint.getElementsByTagName('gpxtpx:atemp')[0].firstChild.data
+    hr = trackpoint.getElementsByTagName('gpxtpx:hr')[0].firstChild.data
+    cad = trackpoint.getElementsByTagName('gpxtpx:cad')[0].firstChild.data
+
+    data = {
+        'lat': float(lat),
+        'lon': float(lon),
+        'ele': float(ele),
+        'atemp': float(atemp),
+        'time': time,
+        'hr': float(hr),
+        'cad': float(cad),
+        'speed': float(speed)
     }
+    return data
 
-    hr = hr_file['hr']
-    times = track['time']
-    datetimes = []
-    for t in times:
-        datetimes.append(datetime.strptime(t, '%Y-%m-%dT%H:%M:%SZ'))
 
-    totaltime = (datetimes[-1] - datetimes[0]).total_seconds()
+def get_dist_gps(dp1, dp2):
+    lat1 = dp1['lat']
+    lon1 = dp1['lon']
+    lat2 = dp2['lat']
+    lon2 = dp2['lon']
+    return distance((lat1, lon1), (lat2, lon2)).km
 
-    for i in range(0,len(datetimes) - 1):
-        cur_hr = hr[i]
-        for name, entry in rate_table.items():
-            (hrmin, hrmax) = entry[0]
-            if hrmin < cur_hr and cur_hr <= hrmax:
-                entry[1] += (datetimes[i+1] - datetimes[i]).total_seconds()
 
-    cum_time = 0 # this is different, because fractional seconds are lost every reading and eventually creates a 2x error!
-    for name, entry in rate_table.items():
-        cum_time += entry[1]
+def _get_dist(dist_inc):
+    dist = [0]
+    for inc in dist_inc[1:]:
+        dist.append(dist[-1] + inc)
+    return dist
 
-    for name, entry in rate_table.items():
-        (hrmin, hrmax) = entry[0]
-        print(name + ' ({:3.0f}-{:3.0f}): '.format(hrmin, hrmax) + str(timedelta(seconds= (entry[1] / cum_time) * totaltime)).split('.')[0] + ' {:.1f}'.format(100.0 * (entry[1] / cum_time)) + '%')
 
-    speeds = track['speed']
-    minima = min(speeds)
-    maxima = max(speeds)
+def gps_dist(flat):
+    dist_inc = [0]
+    for index, dp in enumerate(flat[1:]):
+        dist_inc.append(get_dist_gps(flat[index], dp))
+    return _get_dist(dist_inc)
 
-    norm = matplotlib.colors.Normalize(vmin=minima, vmax=maxima, clip=True)
-    mapper = cm.ScalarMappable(norm=norm, cmap=cm.plasma)
-    m = folium.Map(location=[track['lat'][0], track['lon'][0]], zoom_start=15)
-    elapsed = 0.0
-    cur_interval = 0.0
-    hr_avg_sum = 0.0
-    hr_n = 0
-    for index in range(len(hr) - 1):
-        elapsed += ( datetime.strptime(track['time'][index+1], '%Y-%m-%dT%H:%M:%SZ') - datetime.strptime(track['time'][index], '%Y-%m-%dT%H:%M:%SZ') ).total_seconds()
-        hr_avg_sum += hr[index]
-        hr_n += 1
-        if elapsed >= cur_interval:
-            cur_interval += hr_plot_interval
-            folium.map.Marker(
-                [track['lat'][index], track['lon'][index]],
-                icon=DivIcon(
-                    icon_size=(60,12),
-                    icon_anchor=(0,0),
-                    html='<div style="font-size: 10pt">'+'{:.0f}'.format(hr_avg_sum / hr_n)+'</div>',
-                )
-            ).add_to(m)
-            hr_avg_sum = 0.0
-            hr_n = 0
 
-        if track['speed'][index] == 0:
-            track['speed'][index] = 0.01
+def speed_dist(duration, speeds):
+    dist_inc = [0]
+    for i in range(len(duration) - 1):
+        dt = duration[i+1] - duration[i]
+        d = 1/2*(speeds[i] + speeds[i+1])*dt
+        dist_inc.append(d)
+    dist_meter = _get_dist(dist_inc)
+    dist_km = [1/1000*d for d in dist_meter]
+    return dist_km
 
-        if hr:
-            tooltip="{:0.1f}kph".format(track['speed'][index]) + ' ' + str(hr[index]) +'bpm'
-        else:
-            tooltip="{:0.1f}kph".format(track['speed'][index])
-        folium.CircleMarker(
-            location=(track['lat'][index], track['lon'][index]),
-            radius=((hr[index] - minima) / 50.0)**2,
-            tooltip=tooltip,
-            fill_color=matplotlib.colors.to_hex(mapper.to_rgba(speeds[index])),
-            fill=True,
-            fill_opacity=0.2,
-            weight=0,
-        ).add_to(m)
-
-    m.save(output)
-
-#for track in read_gpx_file('2020-07-23-16-19-52-speed.gpx'):
-#    plot_osm_map(track)
-"""
-  gpsbabel -t -i garmin_fit -x track,speed -f 2020-07-23-16-19-52.fit -o gpx -F 2020-07-23-16-19-52-speed.gpx
-  gpsbabel -t -i garmin_fit -f 2020-07-23-16-19-52.fit -o gpx,garminextensions -F 2020-07-23-16-19-52-hr.gpx
-"""
 
 def main():
-    parser = argparse.ArgumentParser(description="Plot GPX data onto a map")
-    parser.add_argument(
-        "-f", "--file", help="Input file", required=True, type=str,
-    )
-    parser.add_argument(
-        "-r", "--hr-file", help="Heart rate file (must be gpx)", type=str,
-    )
-    parser.add_argument(
-        "-o", "--output", help="Output map name. Defaults to *-map.html", default='map.html', type=str,
-    )
+    # fitfile = './B5CE0704.fit'
+    # convert_fit_to_gpx(fitfile)
 
-    args = parser.parse_args()
-    filename, filextension = os.path.splitext(args.file)
-    hr = None
-    if filextension == '.fit':
-        speedfile='/tmp/speed.gpx'
-        hrfile='/tmp/hr.gpx'
-        subprocess.call(["gpsbabel", "-t", "-i", "garmin_fit", "-x", "track,speed", "-f", args.file, "-o", "gpx", "-F", speedfile])
-        subprocess.call(["gpsbabel", "-t", "-i", "garmin_fit", "-f", args.file, "-o", "gpx,garminextensions", "-F", hrfile])
-        hr = read_hr_bodge(hrfile)
-    else:
-        speedfile=args.file
-        if args.hr_file != None:
-            hr = read_hr_bodge(args.hr_file)
+    gpxfile = './qmapshack_export.gpx'
+    tracks = process_gpx_file(gpxfile)
+    waypointdata = get_waypoints_from_gpx(gpxfile)
 
-    for track in read_gpx_file(speedfile):
-        if hr:
-            plot_osm_map(track, filename + '-speed-' + args.output, hr)
-            plot_osm_hr_map(track, hr, filename + '-hr-' + args.output)
-        else:
-            plot_osm_map(track, filename + '-' + args.output, None)
+    flat = []
+    num_tracks = int(len(tracks))
+    print(f"have {num_tracks} tracks")
+    for track in tracks:
+        num_seg = int(len(track.segments))
+        print(f"Track has {num_seg} segments")
+        for seg in track.segments:
+            flat = flat + seg.datapoints
+
+
+    p1 = tracks[0].segments[0].datapoints[-1]
+    p2 = tracks[0].segments[1].datapoints[0]
+
+
+    datetimes = [datetime.strptime(dp['time'], '%Y-%m-%dT%H:%M:%S.000Z') for dp in flat]
+    total_time = (datetimes[-1] - datetimes[0]).total_seconds()/(60*60)  # in h
+    duration_sec = [ (d - datetimes[0]).total_seconds() for d in datetimes]
+    duration_plot = [mdates.date2num(d) - mdates.date2num(datetimes[0]) for d in datetimes]
+    speeds_mps = [dp['speed'] for dp in flat]
+    speeds_kmh = [3.6*s for s in speeds_mps]  # m/s, need km/h
+    paces = [(16+2/3)/dp['speed'] for dp in flat]
+    elevation = [dp['ele'] for dp in flat]
+
+    dist_gps = gps_dist(flat)
+    dist_speed = speed_dist(duration_sec, speeds_mps)
+    dist = [1/2*(d1 + d2) for d1, d2 in zip(dist_gps, dist_speed)]
+
+
+
+    average_pace = 60/(dist[-1]/total_time)
+    print(f"Length: {dist[-1]:.2f}")
+    d, r = divmod(total_time, 1)
+    r1, r2 = divmod(60*r, 1)
+    print(f"Time: {int(d)}:{int(r1)}:{r2*60:.0f}")
+    d, r = divmod(average_pace, 1)
+    print(f"Average pace {int(d)}:{r*60:.0f}")
+    offset = get_dist_gps(p1, p2)
+    print(f"Offset due to multiple segments {offset}km")
+    average_pace_list = [average_pace for dp in flat]
+
+
+    hr = [dp['hr'] for dp in flat]
+    fig, ax = plt.subplots()
+    # ax.set_ylim([100, 190])
+
+    # ax.xaxis.set_major_formatter(mdates.DateFormatter('%H-%M'))
+    # ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=2))
+    # ax.xaxis.set_minor_locator(mdates.SecondLocator(interval=30))
+    # ax.plot(duration_plot, hr)
+    # ax.plot(duration_plot, speeds_kmh)
+    # ax.plot(duration_plot, elevation)
+    # fig.autofmt_xdate()
+
+    ax.plot(dist, average_pace_list, ls='--')
+    ax.plot(dist, paces)
+
+    # ax.format_xdata = mdates.DateFormatter('%M-%S')
+    plt.show()
 
 
 if __name__ == "__main__":
